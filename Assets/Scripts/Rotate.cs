e6eff5614711d7531ba71dbe116f7f2005503967
Jan Rabe <cloudgazer3d@gmail.com>
2016-04-02 12:53:06 +0200

    added demo scene

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Rotate : MonoBehaviour
+    {
+
+        private float x = 0;
+        private float y = 0;
+        private float z = 0;
+
+        void Update ()
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(++x, ++y, ++z));
+        }
+    }
+}
