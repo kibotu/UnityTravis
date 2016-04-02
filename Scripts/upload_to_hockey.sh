91d51851e23eb6fc28909e4f1a440a7394b435be
Jan Rabe <cloudgazer3d@gmail.com>
2016-04-02 14:46:11 +0200

    fixed upload to hockey script

--- Scripts/upload_to_hockey.sh
+++ Scripts/upload_to_hockey.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-project="ci-build"
-
 curl \
+	--progress-bar \
     -F "status=2" \
     -F "notify=1" \
     -F "notes=CI-Build" \
     -F "notes_type=0" \
-    -F "ipa=@$(pwd)/Build/android/$project.apk" \
-    -H "X-HockeyAppToken: $(HOCKEY_API_TOKEN)" \
-    https://rink.hockeyapp.net/api/2/apps/$(HOCKEY_APP_TOKEN)/app_versions/upload \
-  
+    -F "ipa=@$APP_PATH" \
+    -H "X-HockeyAppToken: $HOCKEY_API_TOKEN" \
+    https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_TOKEN/app_versions/upload \
+	| tee ; test ${PIPESTATUS[0]} -eq 0
