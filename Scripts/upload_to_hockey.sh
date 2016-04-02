7a60a3e57dcbf412bb059f2457b5f615dc2c0620
Jan Rabe <cloudgazer3d@gmail.com>
2016-04-02 14:21:52 +0200

    formatted curl upload to hockey script

--- Scripts/upload_to_hockey.sh
+++ Scripts/upload_to_hockey.sh
@@ -2,9 +2,12 @@
 
 project="ci-build"
 
-curl \ -F "status=2" \
-	-F "notify=1" \ 
-	-F "notes=New Release" \ 
-	-F "notes_type=0" \
-	-F "ipa=@$(pwd)/Build/android/$project.apk" \
-	-H "X-HockeyAppToken: $(HOCKEY_API_TOKEN)" \https://rink.hockeyapp.net/api/2/apps/$(HOCKEY_APP_TOKEN)/app_versions/upload
+curl \
+    -F "status=2" \
+    -F "notify=1" \
+    -F "notes=CI-Build" \
+    -F "notes_type=0" \
+    -F "ipa=@$(pwd)/Build/android/$project.apk" \
+    -H "X-HockeyAppToken: $(HOCKEY_API_TOKEN)" \
+    https://rink.hockeyapp.net/api/2/apps/$(HOCKEY_APP_TOKEN)/app_versions/upload \
+  
