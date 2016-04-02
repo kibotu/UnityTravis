2b22af0f895ab8cfd4fd282025fe73502fdf0668
Jan Rabe <cloudgazer3d@gmail.com>
2016-04-02 15:01:54 +0200

    still can't seem to get the apk path right q.q

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -11,6 +11,8 @@ echo "Attempting to build $project for Windows"
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
+echo "Build stored at:"
+echo "$(pwd)/Build/windows/$project.exe"
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -21,6 +23,8 @@ echo "Attempting to build $project for OS X"
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
+echo "Build stored at:"
+echo "$(pwd)/Build/osx/$project.app"
 
 echo "Attempting to build $project for Android"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -31,16 +35,8 @@ echo "Attempting to build $project for Android"
   -projectPath $(pwd) \
   -buildTarget Android "$(pwd)/Build/android/$project.apk" \
   -quit
-  
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
+echo "Build stored at:"
+echo "$(pwd)/Build/android/$project.apk"
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
